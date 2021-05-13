af540a13f331c668420991e85e829620a759548d
falcao <60127788+falcao-g@users.noreply.github.com>
2021-05-13 09:20:24 -0300

    Add files via upload

---
+++ roll.py
@@ -0,0 +1,41 @@
+import d20
+import discord
+import json
+from discord.ext import commands
+from secret import secret_token
+
+def get_prefix(bot, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    
+    try:
+        return prefixes[str(message.guild.id)]
+    except KeyError:
+        with open('prefixes.json', 'r') as f:
+            prefixes = json.load(f)
+    
+        prefixes[str(message.guild.id)] = '?'
+
+        with open('prefixes.json', 'w') as f:
+            json.dump(prefixes, f, indent=4)
+
+        return prefixes[str(message.guild.id)]
+
+intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True, guild_messages=True)
+bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, intents=intents, help_command=None)
+
+@bot.event
+async def on_message(message):
+    message.content = '?roll' + ' ' + message.content
+    await bot.process_commands(message)
+
+@commands.guild_only()
+@bot.command()
+async def roll(ctx, *roll):
+    dice = ''
+    for c in roll:
+        dice += c
+    result = d20.roll(dice)
+    await ctx.send(f'{ctx.message.author.mention}, {result}')
+
+bot.run(secret_token)
