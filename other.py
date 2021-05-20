fe86444a7ac0b73a512a507ac2fb6586b429207a
falcao <60127788+falcao-g@users.noreply.github.com>
2021-05-20 16:17:17 -0300

    ?sugestao retirado e ?comandos reformulado

--- other.py
+++ other.py
@@ -6,7 +6,6 @@ from math import sqrt
 from random import randint
 from random import choice
 from secret import secret_token
-import d20
 
 def get_prefix(bot, message):
     with open('prefixes.json', 'r') as f:
@@ -28,33 +27,28 @@ def get_prefix(bot, message):
 intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True, guild_messages=True)
 bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, intents=intents, help_command=None)
 
-
-@commands.guild_only()
-@bot.command()
-async def sugestao(ctx, *args):
-    with open('suggestions.txt', 'a') as arquivo:
-        sugestao = ''
-        for c in args:
-            sugestao += ' '
-            sugestao += c
-            sugestao += ' '
-        arquivo.write(f'\n{sugestao}')
-        arquivo.close()
-    await ctx.send(f'{ctx.message.author.mention} sua sugestão foi salva com suscesso, obrigado! :smiling_face_with_3_hearts:')
-
 @commands.guild_only()
 @bot.command()
 @has_permissions(administrator = True)
-async def prefixo(ctx, arg):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
+async def prefixo(ctx, arg=''):
+    if arg == '':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Muda o prefixo do servidor atual (só usuários com a permissão de admin podem usar)', inline=False)
+        embed.add_field(name=f'Uso', value=f'?prefixo <prefixo>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+    else:
+        with open('prefixes.json', 'r') as f:
+            prefixes = json.load(f)
     
-    prefixes[str(ctx.guild.id)] = arg
+        prefixes[str(ctx.guild.id)] = arg
 
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
+        with open('prefixes.json', 'w') as f:
+            json.dump(prefixes, f, indent=4)
     
-    await ctx.send(f'{ctx.message.author.mention} o prefixo do servidor foi mudado para "{arg}"  :smile:')
+        await ctx.send(f'{ctx.message.author.mention} o prefixo do servidor foi mudado para "{arg}"  :smile:')
 
 @commands.guild_only()
 @bot.command()
@@ -97,6 +91,7 @@ async def lena(ctx, ruido, *args):
 @commands.guild_only()
 @bot.command()
 async def luh(ctx):
+    #amor da minha vida <3
     await ctx.message.delete()
     await ctx.send(':two_hearts:')
 
@@ -138,15 +133,6 @@ async def math(ctx, *args):
 
 @commands.guild_only()
 @bot.command()
-async def roll(ctx, *roll):
-    dice = ''
-    for c in roll:
-        dice += c
-    result = d20.roll(dice)
-    await ctx.send(f'{ctx.message.author.mention}, {result}')
-
-@commands.guild_only()
-@bot.command()
 async def coinflip(ctx):
     x = randint(0, 1)
     if x == 0:
@@ -178,44 +164,194 @@ async def bonk(ctx, *args):
 
 @commands.guild_only()
 @bot.command(aliases=['help'])
-async def comandos(ctx):
-    embed = discord.Embed(
-        title='Comandos para sala de jogos',
-        color=discord.Color.green()
-    )
-    embed.add_field(name=f"?eu", value=f'Mostra os seus dados', inline=False)
-    embed.add_field(name=f"?lootbox", value=f'Resgata sua lootbox grátis(disponível a cada 30 minutos)', inline=False)
-    embed.add_field(name=f"?sobre [@pessoa]", value=f'Mostra os dados sobre a pessoa marcada', inline=False)
-    embed.add_field(name="?doar [@pessoa] [valor]", value='Doa o valor inserido para a pessoa marcada (paga sua dívida com ela, caso tenha uma)', inline=False)
-    embed.add_field(name=f"?apostar [valor]", value=f'Aposta o valor ou porcentagem indicado, com ganhos até 100%!', inline=False)
-    embed.add_field(name=f"?duelo [@pessoa] [valor]", value=f'Duela com a pessoa marcada. apostando o valor indicado', inline=False)
-    embed.add_field(name=f"?rank", value=f'Retorna a tabela de ranking do servidor atual por falcoins', inline=False)
-    embed.add_field(name=f"?rank_global", value=f'Retorna a tabela de ranking global por falcoins', inline=False)
-    embed.add_field(name=f"?loja", value=f"Retorna a tabela de compras e seus valores", inline=False)
-    embed.add_field(name=f"?comprar [Número do item]", value=f"Compra o item citado no parametro se você tem os requisitos")
-    embed.add_field(name=f"?investir [@pessoa] [Quantidade]", value=f"Tranfere a quantidade inserida para a pessoa, e ela pagará uma dívida com parte dos ganhos das apostas", inline=False)
-    embed.add_field(name=f'?3', value=f'A cada 24h te dá 3 falcoins!', inline=False)
-    embed.add_field(name=f'Exemplos', value=f'?sobre @Falcão = retorna os dados do usuário Falcão \n ?eu = retorna os seus dados de usuário \n ?lootbox = Resgata a lootbox se disponível \n ?apostar 10 = aposta 10 falcoins, podendo lucar ou perder \n ?duelo @Falcão 100 = aposta 100 falcoins com falcão em um duelo da sorte \n ?rank = Retorna os 10 primeiros em quantidade de falcoins do servidor \n ?rank_global = Retorna os 10 primeiros em quantidade de falcoins global \n ?loja = Retorna a tabela para compras \n ?comprar 1 = Se tiver todos os requisitos, compra o item número 1 \n ?investir @Falcão 200 = Empresta 200 falcoins a falcão, ao quitar a dívida, você ganhará uma porcentagem a mais dependendo do seu cargo \n ?doar @Falcão 100 = doa 100 falcoins para o usuário Falcão', inline=False)
-    embed.set_footer(text='by Falcão ❤️')
-    await ctx.send(embed=embed)
+async def comandos(ctx, command=''):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
 
-    embed1 = discord.Embed(
-        title='Outros comandos',
-        color=discord.Color.red()
-    )
-    embed1.add_field(name=f"?prefixo [Prefixo desejado]", value=f'Muda o prefixo do bot no servidor, OBS: só administradores podem usar', inline=False)
-    embed1.add_field(name=f"?sugestao [...]", value=f'Anota sua sugestão para o bot!', inline=False)
-    embed1.add_field(name=f'?help/?comandos', value=f'Devolve a lista de comandos que você está vendo agora', inline=False)
-    embed1.add_field(name=f'?limpa [num. de msgs]', value=f'Limpa o numero de mensagens especificado no canal atual, OBS: só administradores podem usar', inline=False)
-    embed1.add_field(name=f'?tetris', value=f'Cria uma sala privada no jstris para você!', inline=False)
-    embed1.add_field(name=f'?math', value=f'Faz contas matemáticas para você!', inline=False)
-    embed1.add_field(name=f'?simounao', value=f'Cria uma enquete com sim e não', inline=False)
-    embed1.add_field(name=f'?roll', value=f'Rola um dado de n lados', inline=False)
-    embed1.add_field(name=f'?flipcoin', value=f'Gira uma moeda e retorna cara ou coroa', inline=False)
-    embed1.add_field(name=f'?bonk [pessoa]', value=f'Use para mandar seus amigos para a horny jail!', inline=False)
-    embed1.add_field(name=f'Exemplos', value=f'?prefixo ! = muda o prefixo do Falbot no servidor para ! \n ?Limpa 10 = o bot vai excluir a mensagem invocando o comando juntamente com as 10 anteriores \n ?math 5**5 faz 5 elevado a 5 \n ?roll 5d20 rola cinco dados de 20 lados \n ?bonk @Falcão manda o usuário Falcão para a horny jail')
-    embed1.set_footer(text='by Falcão ❤️')
-    await ctx.send(embed=embed1)
+    command = command.lower()
 
+    if command == 'eu':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra seus Falcoins, Vitorias e Divida, caso tenha uma', inline=False)
+        embed.add_field(name=f'Uso', value=f'?eu')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'sobre':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra os Falcoins, Vitorias e Divida, da pessoa marcada', inline=False)
+        embed.add_field(name=f'Uso', value=f'?sobre @usuario')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'lootbox':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Resgata sua lootbox grátis (disponível a cada 30 minutos)', inline=False)
+        embed.add_field(name=f'Uso', value=f'?lootbox/?lb')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'doar':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Doa x Falcoins para o usuário marcado', inline=False)
+        embed.add_field(name=f'Uso', value=f'?doar @usuario <falcoins>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'apostar':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Aposta x falcoins', inline=False)
+        embed.add_field(name=f'Ganhos', value=f'Até 100%', inline=False)
+        embed.add_field(name=f'Uso', value=f'?apostar <falcoins>/?ap <falcoins>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'duelo':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Desafie seu amigo para um cara ou coroa', inline=False)
+        embed.add_field(name=f'Ganhos', value=f'2x', inline=False)
+        embed.add_field(name=f'Uso', value=f'?duelo @usuario <falcoins>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'rank':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra o rank do servidor atual', inline=False)
+        embed.add_field(name=f'Uso', value=f'?rank')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'rank_global':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra o rank global', inline=False)
+        embed.add_field(name=f'Uso', value=f'?rank_global')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'loja':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra os cargos disponíveis para compra e seus requerimentos', inline=False)
+        embed.add_field(name=f'Uso', value=f'?loja')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'comprar':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Compra o item colocado se tiver todos os requisitos', inline=False)
+        embed.add_field(name=f'Uso', value=f'?comprar <numero do item>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'investir':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Tranfere a quantidade inserida para a pessoa, e ela pagará uma dívida com parte dos ganhos das apostas', inline=False)
+        embed.add_field(name=f'Uso', value=f'?investir @usuario <falcoins>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'prefixo':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Muda o prefixo do servidor atual (só usuários com a permissão de admin podem usar)', inline=False)
+        embed.add_field(name=f'Uso', value=f'?prefixo <prefixo>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'limpa':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Limpa x mensagens do canal atual', inline=False)
+        embed.add_field(name=f'Uso', value=f'?limpa <numero de mensagens>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'tetris':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Cria uma sala privada no Jstris pra você', inline=False)
+        embed.add_field(name=f'Uso', value=f'?tetris')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'math':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Faz um cálculo de matemática', inline=False)
+        embed.add_field(name=f'Uso', value=f'?math <conta>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'simounao':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Cria uma enquete com sim e não', inline=False)
+        embed.add_field(name=f'Uso', value=f'?simounao')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'roll':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Rola dados pra você', inline=False)
+        embed.add_field(name=f'Usos', value=f'?roll <dados>/<dados>')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'flipcoin':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Faz um cara ou coroa', inline=False)
+        embed.add_field(name=f'Usos', value=f'?flipcoin')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    elif command == 'bonk':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Manda alguém para a horny jail', inline=False)
+        embed.add_field(name=f'Usos', value=f'?bonk @usuário')
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
+
+    else:    
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f':game_die: Comandos para a sala de jogos', value='`eu`, `sobre`, `lootbox`, `doar`, `apostar`, `duelo`, `rank`, `rank_global`, `loja`, `comprar`, `investir`', inline=False)
+        embed.add_field(name=f':gear: Outros comandos', value=f'`prefixo`, `comandos/help`, `limpa`, `tetris`, `math`, `simounao`, `roll`, `flipcoin`, `bonk`', inline=False)
+        embed.add_field(name=f'⠀', value=f'O seu prefixo é: **{prefixes[str(ctx.guild.id)]}**', inline=False)
+        embed.add_field(name=f'⠀', value=f'Use **?help <comando>** para obter maiores detalhes de um comando específico', inline=False)
+        embed.set_footer(text='by Falcão ❤️')
+        await ctx.send(embed=embed)
 
 bot.run(secret_token)
