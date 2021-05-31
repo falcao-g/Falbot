2e0a6fe997736cc2dd48293a158313499499f369
falcao <60127788+falcao-g@users.noreply.github.com>
2021-05-31 10:36:42 -0300

    nova função e secret token
    nova função que devolve a descrição detalhada de cada comando, fazendo com que o processo de adicionar um novo comando seja muito mais simples, sem impactar a acessibilidade

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,7 @@
 import json
+import discord
+
+secret_token = 'SECRET_TOKEN'
 
 def cria_banco(pessoa):
     assert pessoa.isnumeric()
@@ -145,3 +148,193 @@ def checa_arquivo(pessoa, campo=''):
         return banco[pessoa]
     else:
         return banco[pessoa][campo]
+
+def explain(command, guild_id=''):
+    command = command.lower()
+
+    if command == 'eu':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra seus Falcoins, Vitorias e Divida, caso tenha uma', inline=False)
+        embed.add_field(name=f'Uso', value=f'?eu')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'sobre':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra os Falcoins, Vitorias e Divida, da pessoa marcada', inline=False)
+        embed.add_field(name=f'Uso', value=f'?sobre @usuario')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'lootbox':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Resgata sua lootbox grátis (disponível a cada 30 minutos)', inline=False)
+        embed.add_field(name=f'Uso', value=f'?lootbox/?lb')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'doar':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Doa x Falcoins para o usuário marcado', inline=False)
+        embed.add_field(name=f'Uso', value=f'?doar @usuario <falcoins>')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'apostar':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Aposta x falcoins', inline=False)
+        embed.add_field(name=f'Ganhos', value=f'Até 100%', inline=False)
+        embed.add_field(name=f'Uso', value=f'?apostar <falcoins>/?ap <falcoins>')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'duelo':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Desafie seu amigo para um cara ou coroa', inline=False)
+        embed.add_field(name=f'Ganhos', value=f'2x', inline=False)
+        embed.add_field(name=f'Uso', value=f'?duelo @usuario <falcoins>')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'rank':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra o rank do servidor atual', inline=False)
+        embed.add_field(name=f'Uso', value=f'?rank')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'rank_global':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra o rank global', inline=False)
+        embed.add_field(name=f'Uso', value=f'?rank_global')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'loja':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Mostra os cargos disponíveis para compra e seus requerimentos', inline=False)
+        embed.add_field(name=f'Uso', value=f'?loja')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'comprar':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Compra o item colocado se tiver todos os requisitos', inline=False)
+        embed.add_field(name=f'Uso', value=f'?comprar <numero do item>')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'investir':
+        embed = discord.Embed(
+            color=discord.Color.green()
+        )
+        embed.add_field(name=f'Info', value=f'Tranfere a quantidade inserida para a pessoa, e ela pagará uma dívida com parte dos ganhos das apostas', inline=False)
+        embed.add_field(name=f'Uso', value=f'?investir @usuario <falcoins>')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'prefixo':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Muda o prefixo do servidor atual (só usuários com a permissão de admin podem usar)', inline=False)
+        embed.add_field(name=f'Uso', value=f'?prefixo <prefixo>')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'limpa':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Limpa x mensagens do canal atual', inline=False)
+        embed.add_field(name=f'Uso', value=f'?limpa <numero de mensagens>')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'tetris':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Cria uma sala privada no Jstris pra você', inline=False)
+        embed.add_field(name=f'Uso', value=f'?tetris')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'math':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Faz um cálculo de matemática', inline=False)
+        embed.add_field(name=f'Uso', value=f'?math <conta>')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'simounao':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Cria uma enquete com sim e não', inline=False)
+        embed.add_field(name=f'Uso', value=f'?simounao')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'roll':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Rola dados pra você', inline=False)
+        embed.add_field(name=f'Usos', value=f'?roll <dados>/<dados>')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'flipcoin':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Faz um cara ou coroa', inline=False)
+        embed.add_field(name=f'Usos', value=f'?flipcoin')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    elif command == 'bonk':
+        embed = discord.Embed(
+            color=discord.Color.red()
+        )
+        embed.add_field(name=f'Info', value=f'Manda alguém para a horny jail', inline=False)
+        embed.add_field(name=f'Usos', value=f'?bonk @usuário')
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
+
+    else:
+        with open('prefixes.json', 'r') as f:
+            prefixes = json.load(f)
+
+        embed = discord.Embed(
+            color=discord.Color.blue()
+        )
+        embed.add_field(name=f':game_die: Comandos para a sala de jogos', value='`eu`, `sobre`, `lootbox`, `doar`, `apostar`, `duelo`, `rank`, `rank_global`, `loja`, `comprar`, `investir`', inline=False)
+        embed.add_field(name=f':gear: Outros comandos', value=f'`prefixo`, `comandos/help`, `limpa`, `tetris`, `math`, `simounao`, `roll`, `flipcoin`, `bonk`', inline=False)
+        embed.add_field(name=f'⠀', value=f'O seu prefixo é: **{prefixes[str(guild_id)]}**', inline=False)
+        embed.add_field(name=f'⠀', value=f'Use **?help <comando>** para obter maiores detalhes de um comando específico', inline=False)
+        embed.set_footer(text='by Falcão ❤️')
+        return embed
