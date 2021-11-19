50e827824bac00ae3b69d16fb27dd015d2da728f
falcao <60127788+falcao-g@users.noreply.github.com>
2021-11-19 11:55:09 -0400

    Atualização 2.1
    mudado para usar changeJSON()

--- commands/caixa.js
+++ commands/caixa.js
@@ -8,9 +8,9 @@ module.exports =  {
     cooldown: '1s',
     guildOnly: true,
     minArgs: 1,
+    testOnly: false,
     expectedArgs: '<quantidade>',
     expectedArgsTypes: ['NUMBER'],
-    syntaxError: 'uso incorreto! faça `{PREFIX}`caixa {ARGUMENTS}',
     options: [{
         name: 'quantidade',
         description: 'quantidade de caixas que você ira abrir',
@@ -19,6 +19,7 @@ module.exports =  {
     }],
     callback: async ({message, interaction, args}) => {
         try {
+            functions.createUser(message ? message.author.id : interaction.user.id)
             args[0] = parseInt(args[0])
         if (await functions.readFile(message ? message.author.id : interaction.user.id, 'Caixas') >= args[0] && await functions.readFile(message ? message.author.id : interaction.user.id, 'Chaves') >= args[0]) {
             if (interaction) {
@@ -32,12 +33,15 @@ module.exports =  {
                     chaves = Math.round(Math.random())
                     caixas = Math.round(Math.random())
                     falcoins = functions.randint(500, 15000)
-                    await functions.changeJSON3(message ? message.author.id : interaction.user.id, 'Chaves', 'Caixas', 'Falcoins', chaves - 1, caixas - 1, falcoins)
+                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Chaves', chaves-1)
+                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Caixas', caixas-1)
+                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Falcoins', falcoins)
                 } else {
                     chaves = 0
                     caixas = 0
                     falcoins = 0
-                    await functions.takeAndTake(message ? message.author.id : interaction.user.id, message ? message.author.id : interaction.user.id, 'Caixas', 'Chaves')
+                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Chaves', -1)
+                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Caixas', -1)   
                 }
                 const embed = new Discord.MessageEmbed()
                 .setColor(await functions.getRoleColor(message ? message : interaction, message ? message.author.id : interaction.user.id))
@@ -58,11 +62,7 @@ module.exports =  {
             return 'você não tem caixas e/ou chaves o suficiente para esta ação! :rage:'
         }
         } catch (error) {
-            if (error.message.includes("Cannot read property 'Falcoins' of undefined")) {
-                return 'registro não encontrado! :face_with_spiral_eyes:\npor favor use /cria para criar seu registro e poder usar os comandos de economia'
-            } else {
-                console.log('Caixa:', error)
-            }
+            console.log('Caixa:', error)
         }
     }
 }
