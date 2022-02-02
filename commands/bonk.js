25ff4febc2c31ff79f8003e58b66f43fbf272ed9
falcao <60127788+falcao-g@users.noreply.github.com>
2022-02-02 12:40:14 -0400

    Atualização 2.2.1

--- commands/bonk.js
+++ commands/bonk.js
@@ -10,32 +10,29 @@ module.exports = {
     minArgs: 1,
     expectedArgs: '<usuario>',
     expectedArgsTypes: ['USER'],
-    syntaxError: 'uso incorreto! faça `{PREFIX}`bonk {ARGUMENTS}',
     options: [{
         name: 'usuario',
         description: 'usuario que vai ser mandado para a horny jail',
         required: true,
         type: Discord.Constants.ApplicationCommandOptionTypes.USER
     }],
-    callback: async ({message, interaction, args}) => {
+    callback: async ({message, interaction, user, args}) => {
         try {
             const embed = new Discord.MessageEmbed()
-            .setColor(await functions.getRoleColor(message ? message : interaction, message ? message.author.id : interaction.user.id))
+            .setColor(await functions.getRoleColor(message ? message : interaction, user.id))
             .setImage('https://i.kym-cdn.com/photos/images/original/002/051/072/a4c.gif')
             .setFooter('by Falcão ❤️')
            if (message) {
-               await message.reply({
-                   embeds: [embed] 
-               })
+               return embed
            } else {
                const member = await functions.getMember(interaction, args[0])
                await interaction.reply({
-                   content: `${member} você foi bonked por ${interaction.user.username}`,
+                   content: `${member} você foi bonked por ${user.username}`,
                    embeds: [embed]
                })
            }
         } catch (error) {
-            console.log('Bonk:', error)
+            console.error(`Bonk: ${error}`)
         }
     }
 }
