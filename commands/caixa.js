cf8ddd47d242e657655a7ac87071618e1fa41ad9
falcao <60127788+falcao-g@users.noreply.github.com>
2022-02-02 12:41:06 -0400

    Atualização 2.2.1

--- commands/caixa.js
+++ commands/caixa.js
@@ -8,7 +8,6 @@ module.exports =  {
     cooldown: '1s',
     guildOnly: true,
     minArgs: 1,
-    testOnly: false,
     expectedArgs: '<quantidade>',
     expectedArgsTypes: ['NUMBER'],
     options: [{
@@ -17,52 +16,39 @@ module.exports =  {
         required: true,
         type: Discord.Constants.ApplicationCommandOptionTypes.NUMBER
     }],
-    callback: async ({message, interaction, args}) => {
+    callback: async ({message, interaction, user, args}) => {
         try {
-            functions.createUser(message ? message.author.id : interaction.user.id)
-            args[0] = parseInt(args[0])
-        if (await functions.readFile(message ? message.author.id : interaction.user.id, 'Caixas') >= args[0] && await functions.readFile(message ? message.author.id : interaction.user.id, 'Chaves') >= args[0]) {
-            if (interaction) {
-                interaction.reply({
-                    content: 'Sucesso! suas caixas já serão abertas :money_mouth:'
-                })
+            functions.createUser(user.id)
+            try {
+                var quantity = await functions.specialArg(args[0], user.id, "Caixas")
+            } catch {
+                return `${args[0]} não é um valor válido... :rage:`
             }
-            for (let i = 0; i < args[0]; i++) {
+        if (await functions.readFile(user.id, 'Caixas') >= quantity && await functions.readFile(user.id, 'Chaves') >= quantity) {
+            caixas = 0
+            chaves = 0
+            falcoins = 0
+            for (let i = 0; i < quantity; i++) {
                 var luck = functions.randint(1, 60)
                 if (luck <= 40) {
-                    chaves = Math.round(Math.random())
-                    caixas = Math.round(Math.random())
-                    falcoins = functions.randint(500, 15000)
-                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Chaves', chaves-1)
-                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Caixas', caixas-1)
-                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Falcoins', falcoins)
-                } else {
-                    chaves = 0
-                    caixas = 0
-                    falcoins = 0
-                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Chaves', -1)
-                    functions.changeJSON(message ? message.author.id : interaction.user.id, 'Caixas', -1)   
-                }
-                const embed = new Discord.MessageEmbed()
-                .setColor(await functions.getRoleColor(message ? message : interaction, message ? message.author.id : interaction.user.id))
-                .addField('Caixa', `Você ganhou ${chaves} chaves\nVocê ganhou ${falcoins} falcoins\nVocê ganhou ${caixas} caixas`)
-                .setFooter('by Falcão ❤️')
-                if (message) {
-                    await message.channel.send({
-                        embeds: [embed]
-                    })
-                } else {
-                    interaction.channel.send({
-                        embeds: [embed]
-                    })
-                }
-                await new Promise(resolve => setTimeout(resolve, 2000));
+                    chaves += Math.round(Math.random())
+                    caixas += Math.round(Math.random())
+                    falcoins += functions.randint(500, 15000)
+                } 
             }
+            functions.changeJSON(user.id, 'Chaves', chaves-quantity)
+            functions.changeJSON(user.id, 'Caixas', caixas-quantity)
+            functions.changeJSON(user.id, 'Falcoins', falcoins)
+            const embed = new Discord.MessageEmbed()
+            .setColor(await functions.getRoleColor(message ? message : interaction, user.id))
+            .addField(`Você abriu ${quantity} caixas`, `Você ganhou ${chaves} chaves\nVocê ganhou ${falcoins} falcoins\nVocê ganhou ${caixas} caixas`)
+            .setFooter('by Falcão ❤️')
+            return embed
         } else {
             return 'você não tem caixas e/ou chaves o suficiente para esta ação! :rage:'
         }
         } catch (error) {
-            console.log('Caixa:', error)
+            console.error(`Caixa: ${error}`)
         }
     }
 }
