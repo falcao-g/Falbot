c41d7f26681499c80313a46ac611f61f9b54f8ba
falcao <60127788+falcao-g@users.noreply.github.com>
2022-02-02 12:43:42 -0400

    Atualização 2.2.1

--- commands/comandos.js
+++ commands/comandos.js
@@ -3,15 +3,16 @@ const functions = require('../functions.js')
 
 module.exports =  {
     aliases: ['ajuda', 'help'],
+    category: 'help',
     description: 'Mostra uma lista de todos os comandos e algumas outras informações',
     slash: 'both',
     cooldown: '1s',
     guildOnly: true,
-    callback: async ({message, interaction, args}) => {
+    callback: async ({args}) => {
         try {
             return await functions.explain(args[0] || '')
         } catch (error) {
-            console.log('comandos:', error)
+            console.error(`Comandos: ${error}`)
         }
     }
 }
