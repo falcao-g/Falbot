022bfe4534ddb6ae492a3e7b77ff58bf9dd0034f
falcao <60127788+falcao-g@users.noreply.github.com>
2022-02-02 12:35:25 -0400

    Atualização 2.2.1

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const Discord = require('discord.js')
 const config = require("./config/config.json")
-const intents = new Discord.Intents(32767)
+const intents = new Discord.Intents(['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'])
 const client = new Discord.Client({ intents })
 const WOKCommands = require('wokcommands')
 const path = require('path')
 const functions = require('./functions.js')
 
 client.on("ready", () => {
+  console.log(`O bot está em ${client.guilds.cache.size} servidores`)
+  console.log(`Tem ${client.users.cache.size} usuários usando o bot`)
+
   new WOKCommands(client, {
     commandsDir: path.join(__dirname, 'commands'),
+    featuresDir: path.join(__dirname, 'features'),
     ignoreBots: true,
     ephemeral: false,
     botOwners: [config.owner],
@@ -26,10 +30,10 @@ client.on("ready", () => {
 
   client.user.setActivity('?comandos | arte by: @kinsallum')
 
-  setInterval(functions.bankInterest, 1000 * 86400)
+  setInterval(functions.bankInterest, 1000 * 300)
 
   setInterval(() => {
     client.user.setActivity('?comandos | arte by: @kinsallum')
-  }, 1000 * 600)
+  }, 1000 * 300)
 })
 client.login(config.TOKEN)
