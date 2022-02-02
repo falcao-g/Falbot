128751e838d8decfe219756b0ab6814b899f6612
falcao <60127788+falcao-g@users.noreply.github.com>
2022-02-02 12:38:45 -0400

    Atualização 2.2.1

--- commands/banco.js
+++ commands/banco.js
@@ -1,10 +1,9 @@
-
 const Discord = require('discord.js')
 const functions = require('../functions.js')
 
 module.exports =  {
     category: 'Economia',
-    description: 'Guarda ou tira seus falcoins do banco',
+    description: 'Guarda ou tira seus falcoins do banco, dinheiro no banco aumenta diariamente.',
     slash: 'both',
     cooldown: '1s',
     guildOnly: true,
@@ -25,34 +24,26 @@ module.exports =  {
         type: Discord.Constants.ApplicationCommandOptionTypes.STRING
     }
     ],
-    callback: async ({message, interaction, args}) => {
+    callback: async ({message, interaction, user, args}) => {
         try {
-            functions.createUser(message ? message.author.id : interaction.user.id)
-            args[0] = args[0].toLowerCase()
+            functions.createUser(user.id)
+            if (message) {args[0] = args[0].toLowerCase()}
             if (args[0] == 'depositar') {
                 try {
-                    var quantity = await functions.specialArg(args[1], message ? message.author.id : interaction.user.id)
+                    var quantity = await functions.specialArg(args[1], user.id, "Falcoins")
                 } catch {
-                    if (message) {
-                        message.reply({
-                            content: `${args[1]} não é um valor válido... :rage:`
-                        })
-                    } else {
-                        interaction.reply({
-                            content: `${args[1]} não é um valor válido... :rage:`
-                        })
-                    }
+                    return `${args[1]} não é um valor válido... :rage:`
                 }
     
-                if (await functions.readFile(message ? message.author.id : interaction.user.id, 'Falcoins') >= quantity && quantity > 0) {
-                    functions.takeAndGive(message ? message.author.id : interaction.user.id, message ? message.author.id : interaction.user.id, 'Falcoins', 'Banco', quantity)
+                if (await functions.readFile(user.id, 'Falcoins') >= quantity && quantity > 0) {
+                    functions.takeAndGive(user.id, user.id, 'Falcoins', 'Banco', quantity)
     
                     const embed = new Discord.MessageEmbed()
                      .setTitle(`Você depositou ${await functions.format(quantity)} falcoins :smiley:`)
-                     .setColor(await functions.getRoleColor(message ? message : interaction, message ? message.author.id : interaction.user.id))
-                     .setAuthor(message ? message.author.username : interaction.user.username, message ? message.author.avatarURL() : interaction.user.avatarURL())
-                     .addField('Saldo atual', `${await functions.format(await functions.readFile(message ? message.author.id : interaction.user.id, "Falcoins"))} falcoins`, inline=false)
-                     .addField('Banco', `você tem ${await functions.format(await functions.readFile(message ? message.author.id : interaction.user.id, "Banco"))} falcoins no banco`)
+                     .setColor(await functions.getRoleColor(message ? message : interaction, user.id))
+                     .setAuthor(user.username, user.avatarURL())
+                     .addField('Saldo atual', `${await functions.format(await functions.readFile(user.id, "Falcoins"))} falcoins`, inline=false)
+                     .addField('Banco', `você tem ${await functions.format(await functions.readFile(user.id, "Banco"))} falcoins no banco`)
                      .setFooter('by Falcão ❤️')
     
                     return embed
@@ -64,20 +55,20 @@ module.exports =  {
                 }
             } else if (args[0] == 'sacar') {
                 try {
-                    var quantity = await functions.specialArgBank(args[1], message ? message.author.id : interaction.user.id)
+                    var quantity = await functions.specialArg(args[1], user.id, "Banco")
                 } catch {
                     return `${args[1]} não é um valor válido... :rage:`
                 }
     
-                if (await functions.readFile(message ? message.author.id : interaction.user.id, 'Banco') >= quantity && quantity > 0) {
-                    functions.takeAndGive(message ? message.author.id : interaction.user.id, message ? message.author.id : interaction.user.id, 'Banco', 'Falcoins', quantity)
+                if (await functions.readFile(user.id, 'Banco') >= quantity && quantity > 0) {
+                    functions.takeAndGive(user.id, user.id, 'Banco', 'Falcoins', quantity)
     
                     const embed = new Discord.MessageEmbed()
                      .setTitle(`Você sacou ${await functions.format(quantity)} falcoins :smiley:`)
-                     .setColor(await functions.getRoleColor(message ? message : interaction, message ? message.author.id : interaction.user.id))
-                     .setAuthor(message ? message.author.username : interaction.user.username, message ? message.author.avatarURL() : interaction.user.avatarURL())
-                     .addField('Saldo atual', `${await functions.format(await functions.readFile(message ? message.author.id : interaction.user.id, "Falcoins"))} falcoins`, inline=false)
-                     .addField('Banco', `você tem ${await functions.format(await functions.readFile(message ? message.author.id : interaction.user.id, "Banco"))} falcoins no banco`)
+                     .setColor(await functions.getRoleColor(message ? message : interaction, user.id))
+                     .setAuthor(user.username, user.avatarU)
+                     .addField('Saldo atual', `${await functions.format(await functions.readFile(user.id, "Falcoins"))} falcoins`, inline=false)
+                     .addField('Banco', `você tem ${await functions.format(await functions.readFile(user.id, "Banco"))} falcoins no banco`)
                      .setFooter('by Falcão ❤️')
     
                      return embed
@@ -90,7 +81,7 @@ module.exports =  {
                 return `${args[0]} não é um valor válido... :rage:`
             }
         } catch (error) {
-            console.log('banco:', error)
+            console.error(`banco: ${error}`)
         }
     }
 }   
