560c9efaf1f8572bfa57591e7add5ddbb000bb65
falcão <gabriel.p.falcao50@gmail.com>
2022-03-04 16:54:00 -0400

    user-schema criado

---
+++ schemas/user-schema.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const reqString = {
+    type: String,
+    required: true
+}
+
+const userSchema = mongoose.Schema({
+    _id: reqString,
+    falcoins: {
+        type: Number,
+        default: 10000
+    },
+    vitorias: {
+        type: Number,
+        default: 0
+    },
+    banco: {
+        type: Number,
+        default: 0
+    },
+    caixas: {
+        type: Number,
+        default: 0
+    },
+    chaves: {
+        type: Number,
+        default: 0
+    },
+    lootbox: {
+        type: Number,
+        default: 1000
+    },
+    lastVote: {
+        type: Number,
+        default: 0
+    }
+}, {
+    timestamps: true
+})
+                                    
+module.exports = mongoose.model('users', userSchema)
