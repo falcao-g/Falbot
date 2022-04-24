9f1d9744774abc30aaf223372b316fd3f37280eb
Falcão <gabriel.p.falcao50@gmail.com>
2022-04-24 02:31:25 -0400

    rescrita quase completa
    agora o banco usa sub comandos para ser mais intuitivo para o usuário, também mudei a estrutura para um switch e não é mais necessário importar as constantes do discord.js

--- src/commands/bank.js
+++ src/commands/bank.js
@@ -1,4 +1,4 @@
-const {Constants, MessageEmbed} = require('discord.js')
+const {MessageEmbed} = require('discord.js')
 const {specialArg, readFile, takeAndGive, format, getRoleColor} = require('../utils/functions.js')
 const {testOnly} = require("../config.json")
 
@@ -12,70 +12,79 @@ module.exports =  {
     minArgs: 2,
     testOnly,
     expectedArgs: '<option> <falcoins>',
-    expectedArgsTypes: ['STRING', 'STRING'],
-    options: [{
-        name:'option',
-        description: 'if you are going to deposit or withdraw falcoins',
-        required: true,
-        type: Constants.ApplicationCommandOptionTypes.STRING,
-        choices: [{name: 'deposit', value: 'deposit'}, {name: 'withdraw', value: 'withdraw'}]
+    expectedArgsTypes: ['SUB_COMMAND', 'STRING'],
+    options: [
+    {
+        name:'deposit',
+        description: 'deposit falcoins to the bank',
+        type: "SUB_COMMAND",
+        options: [{name: 'falcoins', description: 'the amount of falcoins to deposit', type: "STRING", required: true}]
     },
     {
-        name: 'falcoins',
-        description: 'the amount of falcoins you want to deposit or withdraw',
-        required: true,
-        type: Constants.ApplicationCommandOptionTypes.STRING
+        name:'withdraw',
+        description: 'withdraw falcoins from the bank',
+        type: "SUB_COMMAND",
+        options: [{name: 'falcoins', description: 'the amount of falcoins to withdraw', type: "STRING", required: true}]
     }
     ],
-    callback: async ({instance, guild, message, user, args}) => {
+    callback: async ({instance, guild, message, user, args, interaction}) => {
         try {
-            if (message) {args[0] = args[0].toLowerCase()}
-            if (args[0] == 'depositar' || args[0] == 'deposit') {
-                try {
-                    var quantity = await specialArg(args[1], user.id, "falcoins")
-                } catch {
-                    return instance.messageHandler.get(guild, "VALOR_INVALIDO", {VALUE: args[1]})
-                }
-    
-                if (await readFile(user.id, 'falcoins') >= quantity && quantity > 0) {
-                    await takeAndGive(user.id, user.id, 'falcoins', 'banco', quantity)
-    
-                    const embed = new MessageEmbed()
-                     .setTitle(instance.messageHandler.get(guild, "BANCO_DEPOSITOU", {VALUE: await format(quantity)}))
-                     .setColor(await getRoleColor(guild, user.id))
-                     .setAuthor({name: user.username, iconURL: user.avatarURL()})
-                     .addField(instance.messageHandler.get(guild, "SALDO_ATUAL"), `${await readFile(user.id, "falcoins", true)} falcoins`, inline=false)
-                     .addField(instance.messageHandler.get(guild, "BANCO"), instance.messageHandler.get(guild, "BANCO_SALDO", {VALUE: await readFile(user.id, "banco", true)}))
-                     .setFooter({text: 'by Falcão ❤️'})
-    
-                    return embed
-                } else {
-                    return instance.messageHandler.get(guild, "FALCOINS_INSUFICIENTES")
-                }
-            } else if (args[0] == 'sacar' || args[0] == 'withdraw') {
-                try {
-                    var quantity = await specialArg(args[1], user.id, "Banco")
-                } catch {
-                    return instance.messageHandler.get(guild, "VALOR_INVALIDO", {VALUE: args[1]})
-                }
-    
-                if (await readFile(user.id, 'banco') >= quantity && quantity > 0) {
-                    await takeAndGive(user.id, user.id, 'banco', 'falcoins', quantity)
-    
-                    const embed = new MessageEmbed()
-                     .setTitle(instance.messageHandler.get(guild, "BANCO_SACOU", {VALUE: await format(quantity)}))
-                     .setColor(await getRoleColor(guild, user.id))
-                     .setAuthor({name: user.username, iconURL: user.avatarURL()})
-                     .addField(instance.messageHandler.get(guild, "SALDO_ATUAL"), `${await readFile(user.id, "falcoins", true)} falcoins`, inline=false)
-                     .addField(instance.messageHandler.get(guild, "BANCO"), instance.messageHandler.get(guild, "BANCO_SALDO", {VALUE: await readFile(user.id, "banco", true)}))
-                     .setFooter({text: 'by Falcão ❤️'})
-    
-                     return embed
-                } else {
-                    return instance.messageHandler.get(guild, "BANCO_INSUFICIENTE")
-                }
+            if (message) {
+                metodo = args[0].toLowerCase()
+                falcoins = args[1]
             } else {
-                return instance.messageHandler.get(guild, "VALOR_INVALIDO", {VALUE: args[0]})
+                metodo = interaction.options.getSubcommand()
+                falcoins = interaction.options.getNumber('falcoins')
+            }
+            switch (metodo) {
+                case 'depositar':
+                case 'deposit': 
+                    try {
+                        var quantity = await specialArg(falcoins, user.id, "falcoins")
+                    } catch {
+                        return instance.messageHandler.get(guild, "VALOR_INVALIDO", {VALUE: falcoins})
+                    }
+        
+                    if (await readFile(user.id, 'falcoins') >= quantity && quantity > 0) {
+                        await takeAndGive(user.id, user.id, 'falcoins', 'banco', quantity)
+        
+                        const embed = new MessageEmbed()
+                        .setTitle(instance.messageHandler.get(guild, "BANCO_DEPOSITOU", {VALUE: await format(quantity)}))
+                        .setColor(await getRoleColor(guild, user.id))
+                        .setAuthor({name: user.username, iconURL: user.avatarURL()})
+                        .addField(instance.messageHandler.get(guild, "SALDO_ATUAL"), `${await readFile(user.id, "falcoins", true)} falcoins`, inline=false)
+                        .addField(instance.messageHandler.get(guild, "BANCO"), instance.messageHandler.get(guild, "BANCO_SALDO", {VALUE: await readFile(user.id, "banco", true)}))
+                        .setFooter({text: 'by Falcão ❤️'})
+        
+                        return embed
+                    } else {
+                        return instance.messageHandler.get(guild, "FALCOINS_INSUFICIENTES")
+                    }
+                case 'sacar':
+                case 'withdraw':
+                    try {
+                        var quantity = await specialArg(falcoins, user.id, "Banco")
+                    } catch {
+                        return instance.messageHandler.get(guild, "VALOR_INVALIDO", {VALUE: falcoins})
+                    }
+        
+                    if (await readFile(user.id, 'banco') >= quantity && quantity > 0) {
+                        await takeAndGive(user.id, user.id, 'banco', 'falcoins', quantity)
+        
+                        const embed = new MessageEmbed()
+                         .setTitle(instance.messageHandler.get(guild, "BANCO_SACOU", {VALUE: await format(quantity)}))
+                         .setColor(await getRoleColor(guild, user.id))
+                         .setAuthor({name: user.username, iconURL: user.avatarURL()})
+                         .addField(instance.messageHandler.get(guild, "SALDO_ATUAL"), `${await readFile(user.id, "falcoins", true)} falcoins`, inline=false)
+                         .addField(instance.messageHandler.get(guild, "BANCO"), instance.messageHandler.get(guild, "BANCO_SALDO", {VALUE: await readFile(user.id, "banco", true)}))
+                         .setFooter({text: 'by Falcão ❤️'})
+        
+                         return embed
+                    } else {
+                        return instance.messageHandler.get(guild, "BANCO_INSUFICIENTE")
+                    }
+                default:
+                    return instance.messageHandler.get(guild, "VALOR_INVALIDO", {VALUE: args[0]})
             }
         } catch (error) {
             console.error(`bank: ${error}`)
