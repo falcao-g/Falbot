d734566fae4b9682919cc9fc07e9179beb0bb462
Falcão <gabriel.p.falcao50@gmail.com>
2022-06-16 20:40:24 -0400

    leaderboard by rank added

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -13,43 +13,76 @@ module.exports =  {
     expectedArgs: '[scope]',
     expectedArgsTypes: ['SUB_COMMAND'],
     options: [{
-        name:'local',
-        description: 'show this server\'s falcoins ranking',
+        name:'money',
+        description: 'View users ranked by falcoins',
         type: "SUB_COMMAND",
-    },
-    {
-        name:'global',
-        description: 'show the global falcoins ranking',
+        options: [{name: "type", description: "server or global", type: "STRING", choices: [{name: "server", value: "server"}, {name: "global", value: "global"}], required: true}]
+    }, {
+        name:'rank',
+        description: 'View users ranked by ranks',
         type: "SUB_COMMAND",
+        options: [{name: "type", description: "server or global", type: "STRING", choices: [{name: "server", value: "server"}, {name: "global", value: "global"}], required: true}]
     }
     ],
-    callback: async ({client, user, guild, args, interaction}) => {
+    callback: async ({client, user, guild, args, interaction, instance}) => {
         try {
             rank = []
-            if (!interaction) {scope = args[0].toLowerCase()} else {scope = interaction.options.getSubcommand()}
-            if (scope === 'local') {
-                users = await userSchema.find({}).sort({ 'falcoins': -1 }).limit(10)
-
-                for (useri of users) {
-                    if (await getMember(guild, useri['_id']) &&  rank.length < 10) {
-                        rank.push(useri)
+            if (!interaction) {scope = args[1].toLowerCase()} else {scope = interaction.options.getString("type").toLowerCase()}
+            if (!interaction) {type = args[0].toLowerCase()} else {type = interaction.options.getSubcommand()}
+            
+            switch (type) {
+                case 'money':
+                case 'dinheiro':
+                    if (scope === 'server') {
+                        users = await userSchema.find({}).sort({ 'falcoins': -1 }).limit(10)
+        
+                        for (useri of users) {
+                            if (await getMember(guild, useri['_id']) &&  rank.length < 10) {
+                                rank.push(useri)
+                            }
+                        }
+                    } else {
+                        rank = await userSchema.find({}).sort({ 'falcoins': -1 }).limit(10)
                     }
-                }
-            } else {
-                rank = await userSchema.find({}).sort({ 'falcoins': -1 }).limit(10)
+                    var embed = new MessageEmbed()
+                    .setColor(await getRoleColor(guild, user.id))
+                    .setFooter({text: 'by Falcão ❤️'})
+                    for (let i = 0; i < rank.length; i++) {
+                        try {
+                            user = await client.users.fetch(rank[i]['_id'])
+                            embed.addField(`${i + 1}º - ${user.username} falcoins:`, `${await format(rank[i]['falcoins'])}`, false)
+                        } catch {
+                            embed.addField(`${i + 1}º - Unknown user falcoins:`, `${await format(rank[i]['falcoins'])}`, false)
+                        }
+                    }
+                    return embed
+                case 'rank':
+                    if (scope === 'server') {
+                        users = await userSchema.find({}).sort({ 'rank': -1 }).limit(10)
+        
+                        for (useri of users) {
+                            if (await getMember(guild, useri['_id']) &&  rank.length < 10) {
+                                rank.push(useri)
+                            }
+                        }
+                    } else {
+                        rank = await userSchema.find({}).sort({ 'rank': -1 }).limit(10)
+                    }
+                    var embed = new MessageEmbed()
+                    .setColor(await getRoleColor(guild, user.id))
+                    .setFooter({text: 'by Falcão ❤️'})
+                    for (let i = 0; i < rank.length; i++) {
+                        try {
+                            user = await client.users.fetch(rank[i]['_id'])
+                            embed.addField(`${i + 1}º - ${user.username} rank:`, `${instance.messageHandler.get(guild, rank[i]['rank'])}`, false)
+                        } catch {
+                            embed.addField(`${i + 1}º - Unknown user rank:`, `${instance.messageHandler.get(guild, rank[i]['rank'])}`, false)
+                        }
+                    }
+                    return embed
+                default:
+                    return instance.messageHandler.get(guild, "VALOR_INVALIDO", {VALUE: args[0]})
             }
-            const embed = new MessageEmbed()
-            .setColor(await getRoleColor(guild, user.id))
-            .setFooter({text: 'by Falcão ❤️'})
-            for (let i = 0; i < rank.length; i++) {
-                try {
-                    user = await client.users.fetch(rank[i]['_id'])
-                    embed.addField(`${i + 1}º - ${user.username} falcoins:`, `${await format(rank[i]['falcoins'])}`, false)
-                } catch {
-                    embed.addField(`${i + 1}º - Unknown user falcoins:`, `${await format(rank[i]['falcoins'])}`, false)
-                }
-            }
-            return embed
         } catch (error) {
                 console.error(`rank: ${error}`)
         }
