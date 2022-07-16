817d9a2a190b62845d2d688a34be4886fef941ed
Falcão <gabriel.p.falcao50@gmail.com>
2022-07-16 16:19:41 -0400

    new leaderboard by wins

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -22,6 +22,11 @@ module.exports =  {
         description: 'View users ranked by ranks',
         type: "SUB_COMMAND",
         options: [{name: "type", description: "server or global", type: "STRING", choices: [{name: "server", value: "server"}, {name: "global", value: "global"}], required: true}]
+    }, {
+        name:'wins',
+        description: 'View users ranked by wins',
+        type: "SUB_COMMAND",
+        options: [{name: "type", description: "server or global", type: "STRING", choices: [{name: "server", value: "server"}, {name: "global", value: "global"}], required: true}]
     }
     ],
     callback: async ({client, user, guild, args, interaction, instance}) => {
@@ -80,6 +85,31 @@ module.exports =  {
                         }
                     }
                     return embed
+
+                    case 'wins':
+                        if (scope === 'server') {
+                            users = await userSchema.find({}).sort({ 'vitorias': -1 }).limit(10)
+            
+                            for (useri of users) {
+                                if (await getMember(guild, useri['_id']) &&  rank.length < 10) {
+                                    rank.push(useri)
+                                }
+                            }
+                        } else {
+                            rank = await userSchema.find({}).sort({ 'vitorias': -1 }).limit(10)
+                        }
+                        var embed = new MessageEmbed()
+                        .setColor(await getRoleColor(guild, user.id))
+                        .setFooter({text: 'by Falcão ❤️'})
+                        for (let i = 0; i < rank.length; i++) {
+                            try {
+                                user = await client.users.fetch(rank[i]['_id'])
+                                embed.addField(`${i + 1}º - ${user.username} `  + instance.messageHandler.get(guild, "VITORIAS").toLowerCase() + ':', `${await format(rank[i]['vitorias'])}`, false)
+                            } catch {
+                                embed.addField(`${i + 1}º - Unknown user ` + instance.messageHandler.get(guild, "VITORIAS").toLowerCase() + ':', `${await format(rank[i]['vitorias'])}`, false)
+                            }
+                        }
+                        return embed
                 default:
                     return instance.messageHandler.get(guild, "VALOR_INVALIDO", {VALUE: args[0]})
             }
