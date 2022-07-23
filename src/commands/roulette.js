ac782ce3bb944d95198ed91a4720ddabc5134142
Falcão <gabriel.p.falcao50@gmail.com>
2022-07-23 16:58:23 -0300

    roulette reworked

--- src/commands/roulette.js
+++ src/commands/roulette.js
@@ -1,5 +1,5 @@
 const {MessageEmbed} = require('discord.js')
-const {specialArg, readFile, randint, changeDB, format} = require('../utils/functions.js')
+const {specialArg, readFile, randint, changeDB, format, getRoleColor} = require('../utils/functions.js')
 const {testOnly} = require("../config.json")
 
 module.exports =  {
@@ -18,7 +18,7 @@ module.exports =  {
         description: 'all bets have a 1-1 payout ratio, except green that has a 35-1 because you bet on a single number',
         required: true,
         type: "STRING",
-        choices: [{name: "black", value: "black"}, {name: "red", value: "red"}, {name: "green", value: "green"}, {name: "high", value: "high"}, {name: "low", value: "low"}, {name: "even", value: "even"}, {name: "odd", value: "odd"}]
+        choices: [{name: "black", value: "black"}, {name: "red", value: "red"}, {name: "green", value: "green"}, {name: "high", value: "high"}, {name: "low", value: "low"}, {name: "even", value: "even"}, {name: "odd", value: "odd"}, {name: "1st column", value: "first"}, {name: "2nd column", value: "second"}, {name: "3rd column", value: "third"}]
     },
     {
         name: 'falcoins',
@@ -27,12 +27,12 @@ module.exports =  {
         type: "STRING"
     }
     ],
-    callback: async ({instance, guild, user, args}) => {
+    callback: async ({instance, guild, user, args, message, interaction}) => {
         try {
             args[0] = args[0].toLowerCase()
             if (args[0] === 'ímpar') {args[0] = 'impar'}
     
-            if (args[0] == 'preto' || args[0] == 'black' || args[0] == 'vermelho' || args[0] == 'red' || args[0] == 'verde' || args[0] == 'green' || args[0] == 'altos' || args[0] == 'high' || args[0] == 'baixos' || args[0] == 'low' || args[0] == 'par' || args[0] == 'even' || args[0] == 'impar' || args[0] == 'odd') {
+            if (args[0] == 'preto' || args[0] == 'black' || args[0] == 'vermelho' || args[0] == 'red' || args[0] == 'verde' || args[0] == 'green' || args[0] == 'altos' || args[0] == 'high' || args[0] == 'baixos' || args[0] == 'low' || args[0] == 'par' || args[0] == 'even' || args[0] == 'impar' || args[0] == 'odd' || args[0] == 'first' || args[0] == 'second' || args[0] == 'third') {
                 try {
                     var bet = await specialArg(args[1], user.id, "falcoins")
                 } catch {
@@ -41,6 +41,25 @@ module.exports =  {
 
                 if (await readFile(user.id, 'falcoins') >= bet && bet > 0) {
                     await changeDB(user.id, 'falcoins', -bet)
+
+                    const embed = new MessageEmbed()
+                    .setTitle(instance.messageHandler.get(guild, "ROLETA"))
+                    .setDescription(instance.messageHandler.get(guild, "GIRANDO_ROLETA"))
+                    .setColor(await getRoleColor(guild, user.id))
+                    .setImage('https://media3.giphy.com/media/26uf2YTgF5upXUTm0/giphy.gif')
+                    .setFooter({text: 'by Falcão ❤️'})
+                   
+                   if (message) {
+                       answer = await message.reply({
+                           embeds: [embed]
+                       })
+                   } else {
+                       answer = await interaction.reply({
+                           embeds: [embed],
+                           fetchReply: true
+                       })
+                   }
+
                     const types = {
                         verde: [0],
                         green: [0],
@@ -55,21 +74,29 @@ module.exports =  {
                         impar: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35],
                         odd: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35],
                         par: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36],
-                        even: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36]
+                        even: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36],
+                        first: [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34],
+                        second: [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35],
+                        third: [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
                     }
                     
                     var type = types[args[0]]
                     if (type === "verde" || type === "green") {
-                        var profit = bet * 37
-                    } else {
+                        var profit = bet * 36
+                    } else if (type === 'first' || type === 'second' || type === 'third') {
+                        var profit = bet * 3
+                    } else{
                         var profit = bet * 2
                     }
     
                     const luck = randint(0, 36)
+
+                    await new Promise(resolve => setTimeout(resolve, 3000));
     
                     if (type.includes(luck)) {
                         await changeDB(user.id, 'falcoins', profit)
-                        var embed = new MessageEmbed()
+                        var embed2 = new MessageEmbed()
+                         .setTitle(instance.messageHandler.get(guild, "ROLETA"))
                          .setColor(3066993)
                          .setAuthor({name: user.username, iconURL: user.avatarURL()})
                          .addFields({
@@ -83,7 +110,8 @@ module.exports =  {
                          })
                          .addField(instance.messageHandler.get(guild, "SALDO_ATUAL"), `${await readFile(user.id, 'falcoins', true)} falcoins`, false)
                     } else {
-                        var embed = new MessageEmbed()
+                        var embed2 = new MessageEmbed()
+                         .setTitle(instance.messageHandler.get(guild, "ROLETA"))
                          .setColor(15158332)
                          .setAuthor({name: user.username, iconURL: user.avatarURL()})
                          .addFields({
@@ -95,10 +123,12 @@ module.exports =  {
                              value:`${await format(bet)} falcoins`,
                              inline: true
                         })
-                        embed.addField(instance.messageHandler.get(guild, "SALDO_ATUAL"), `${await readFile(user.id, 'falcoins', true)} falcoins`, false)
+                        .addField(instance.messageHandler.get(guild, "SALDO_ATUAL"), `${await readFile(user.id, 'falcoins', true)} falcoins`, false)
                     }
-                    embed.setFooter({text: 'by Falcão ❤️'})
-                    return embed
+                    embed2.setFooter({text: 'by Falcão ❤️'})
+                    await answer.edit({
+                        embeds: [embed2]
+                    })
                 } else {
                     return instance.messageHandler.get(guild, "FALCOINS_INSUFICIENTES")
                 }
