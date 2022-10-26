95f28fc6f3abb75297ffd9787f6a69f9d710d722
Falcão <gabriel.p.falcao50@gmail.com>
2022-10-25 22:13:45 -0400

    lottery added to cooldowns

--- src/commands/cooldowns.js
+++ src/commands/cooldowns.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require("discord.js")
 const { readFile, getRoleColor, msToTime } = require("../utils/functions.js")
 const { testOnly } = require("../config.json")
+const fs = require("fs")
 
 module.exports = {
 	category: "uteis",
@@ -11,6 +12,7 @@ module.exports = {
 	testOnly,
 	callback: async ({ instance, guild, user, member }) => {
 		try {
+			var config = JSON.parse(fs.readFileSync("./src/config.json", "utf8"))
 			voteCooldown = Date.now() - (await readFile(user.id, "lastVote"))
 			scratchSchema = instance._mongoConnection.models["wokcommands-cooldowns"]
 			scratchCooldown = await scratchSchema.findById(
@@ -21,25 +23,45 @@ module.exports = {
 				.setAuthor({ name: member.displayName, iconURL: user.avatarURL() })
 				.setFooter({ text: "by Falcão ❤️" })
 				.setTitle(instance.messageHandler.get(guild, "COOLDOWNS"))
-				.addField(
-					":ballot_box: " + instance.messageHandler.get(guild, "VOTO"),
-					`**${
-						voteCooldown < 43200000
-							? `:red_circle: ${await msToTime(43200000 - voteCooldown)}`
-							: `:green_circle: ${instance.messageHandler.get(guild, "PRONTO")}`
-					}**`,
-					true
-				)
-				.addField(
-					":slot_machine: " + instance.messageHandler.get(guild, "SCRATCH"),
-					`**${
-						scratchCooldown
-							? `:red_circle: ${await msToTime(
-									scratchCooldown["cooldown"] * 1000
-							  )}`
-							: `:green_circle: ${instance.messageHandler.get(guild, "PRONTO")}`
-					}**`,
-					true
+				.addFields(
+					{
+						name: ":ballot_box: " + instance.messageHandler.get(guild, "VOTO"),
+						value: `**${
+							voteCooldown < 43200000
+								? `:red_circle: ${await msToTime(43200000 - voteCooldown)}`
+								: `:green_circle: ${instance.messageHandler.get(
+										guild,
+										"PRONTO"
+								  )}`
+						}**`,
+						inline: true,
+					},
+					{
+						name:
+							":slot_machine: " + instance.messageHandler.get(guild, "SCRATCH"),
+						value: `**${
+							scratchCooldown
+								? `:red_circle: ${await msToTime(
+										scratchCooldown["cooldown"] * 1000
+								  )}`
+								: `:green_circle: ${instance.messageHandler.get(
+										guild,
+										"PRONTO"
+								  )}`
+						}**`,
+						inline: true,
+					},
+					{
+						name:
+							":loudspeaker: " + instance.messageHandler.get(guild, "EVENTS"),
+						value: `**${instance.messageHandler.get(
+							guild,
+							"LOTTERY"
+						)}** - ${instance.messageHandler.get(guild, "LOTTERY_DRAWN", {
+							TIME: await msToTime(config["lottery"]["drawTime"] - Date.now()),
+						})}`,
+						inline: false,
+					}
 				)
 
 			return embed
