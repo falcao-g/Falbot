eec54b3e467597043c452df56ac5ab271d39e038
Falcão <gabriel.p.falcao50@gmail.com>
2023-01-16 16:02:38 -0400

    further tweaks

--- src/commands/horse.js
+++ src/commands/horse.js
@@ -54,100 +54,74 @@ module.exports = {
 			}
 			if ((await readFile(user.id, "falcoins")) >= bet && bet > 0) {
 				await changeDB(user.id, "falcoins", -bet)
-				if (args[0] >= 1 && args[0] <= 5) {
-					let horse1 = "- - - - -"
-					let horse2 = "- - - - -"
-					let horse3 = "- - - - -"
-					let horse4 = "- - - - -"
-					let horse5 = "- - - - -"
-					const embed = new MessageEmbed()
-						.setDescription(
-							instance.messageHandler.get(guild, "CAVALO_DESCRIPTION", {
-								BET: await format(bet),
-								HORSE: args[0],
-							})
-						)
-						.addFields({
-							name: "\u200b",
-							value: `**1.** :checkered_flag:  ${horse1} :horse_racing:\n\u200b\n**2.** :checkered_flag:  ${horse2} :horse_racing:\n\u200b\n**3.** :checkered_flag:  ${horse3} :horse_racing:\n\u200b\n**4.** :checkered_flag:  ${horse4} :horse_racing:\n\u200b\n**5.** :checkered_flag:  ${horse5}  :horse_racing:`,
+				const horses = [
+					"- - - - -",
+					"- - - - -",
+					"- - - - -",
+					"- - - - -",
+					"- - - - -",
+				]
+				const embed = new MessageEmbed()
+					.setDescription(
+						instance.messageHandler.get(guild, "CAVALO_DESCRIPTION", {
+							BET: await format(bet),
+							HORSE: args[0],
 						})
-						.setColor(await getRoleColor(guild, user.id))
-						.setFooter({ text: "by Falcão ❤️" })
+					)
+					.addFields({
+						name: "\u200b",
+						value: `**1.** :checkered_flag:  ${horses[0]} :horse_racing:\n\u200b\n**2.** :checkered_flag:  ${horses[1]} :horse_racing:\n\u200b\n**3.** :checkered_flag:  ${horses[2]} :horse_racing:\n\u200b\n**4.** :checkered_flag:  ${horses[3]} :horse_racing:\n\u200b\n**5.** :checkered_flag:  ${horses[4]}  :horse_racing:`,
+					})
+					.setColor(await getRoleColor(guild, user.id))
+					.setFooter({ text: "by Falcão ❤️" })
 
+				await interaction.editReply({
+					embeds: [embed],
+				})
+
+				for (let i = 0; i <= 21; i++) {
+					let run = randint(0, 4)
+					horses[run] = horses[run].slice(0, -2)
+
+					embed.fields[0] = {
+						name: "\u200b",
+						value: `**1.** :checkered_flag:  ${horses[0]} :horse_racing:\n\u200b\n**2.** :checkered_flag:  ${horses[1]} :horse_racing:\n\u200b\n**3.** :checkered_flag:  ${horses[2]} :horse_racing:\n\u200b\n**4.** :checkered_flag:  ${horses[3]} :horse_racing:\n\u200b\n**5.** :checkered_flag:  ${horses[4]} :horse_racing:`,
+					}
 					await interaction.editReply({
 						embeds: [embed],
 					})
 
-					for (let i = 0; i <= 21; i++) {
-						let run = randint(1, 5)
-						if (run === 1) {
-							horse1 = horse1.slice(0, -2)
-						} else if (run === 2) {
-							horse2 = horse2.slice(0, -2)
-						} else if (run === 3) {
-							horse3 = horse3.slice(0, -2)
-						} else if (run === 4) {
-							horse4 = horse4.slice(0, -2)
-						} else {
-							horse5 = horse5.slice(0, -2)
-						}
-
-						embed.fields[0] = {
-							name: "\u200b",
-							value: `**1.** :checkered_flag:  ${horse1} :horse_racing:\n\u200b\n**2.** :checkered_flag:  ${horse2} :horse_racing:\n\u200b\n**3.** :checkered_flag:  ${horse3} :horse_racing:\n\u200b\n**4.** :checkered_flag:  ${horse4} :horse_racing:\n\u200b\n**5.** :checkered_flag:  ${horse5} :horse_racing:`,
-						}
-						await interaction.editReply({
-							embeds: [embed],
-						})
-
-						if (
-							horse1 === "" ||
-							horse2 === "" ||
-							horse3 === "" ||
-							horse4 === "" ||
-							horse5 === ""
-						) {
-							break
-						}
-						await new Promise((resolve) => setTimeout(resolve, 250))
+					if (horses[run] === "") {
+						var winner = String(run + 1)
+						break
 					}
 
-					if (horse1 === "") {
-						winner = "1"
-					} else if (horse2 === "") {
-						winner = "2"
-					} else if (horse3 === "") {
-						winner = "3"
-					} else if (horse4 === "") {
-						winner = "4"
-					} else {
-						winner = "5"
-					}
-
-					if (args[0] == winner) {
-						await changeDB(user.id, "falcoins", bet * 5)
-						embed.setColor(3066993).setDescription(
-							instance.messageHandler.get(guild, "CAVALO_DESCRIPTION_WON", {
-								BET: await format(bet),
-								HORSE: args[0],
-								FALCOINS: await format(bet * 5),
-								SALDO: await readFile(user.id, "falcoins", true),
-							})
-						)
-					} else {
-						embed.setColor(15158332).setDescription(
-							instance.messageHandler.get(guild, "CAVALO_DESCRIPTION_LOST", {
-								BET: await format(bet),
-								HORSE: args[0],
-								SALDO: await readFile(user.id, "falcoins", true),
-							})
-						)
-					}
-
-					await interaction.editReply({
-						embeds: [embed],
-					})
+					await new Promise((resolve) => setTimeout(resolve, 250))
 				}
+
+				if (args[0] == winner) {
+					await changeDB(user.id, "falcoins", bet * 5)
+					embed.setColor(3066993).setDescription(
+						instance.messageHandler.get(guild, "CAVALO_DESCRIPTION_WON", {
+							BET: await format(bet),
+							HORSE: args[0],
+							FALCOINS: await format(bet * 5),
+							SALDO: await readFile(user.id, "falcoins", true),
+						})
+					)
+				} else {
+					embed.setColor(15158332).setDescription(
+						instance.messageHandler.get(guild, "CAVALO_DESCRIPTION_LOST", {
+							BET: await format(bet),
+							HORSE: args[0],
+							SALDO: await readFile(user.id, "falcoins", true),
+						})
+					)
+				}
+
+				await interaction.editReply({
+					embeds: [embed],
+				})
 			} else {
 				await interaction.editReply({
 					content: instance.messageHandler.get(guild, "FALCOINS_INSUFICIENTES"),
