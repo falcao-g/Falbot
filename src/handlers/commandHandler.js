e671d17c1a58bcc00d8296d3a2a6f4687b42bf34
Falcão <gabriel.p.falcao50@gmail.com>
2023-02-08 19:06:45 -0400

    testOnly makes all commands only show in one guild

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -6,35 +6,41 @@ async function loadCommands(instance, client) {
 	await client.commands.clear()
 
 	let commandsArray = []
+	let commandsGuild = []
 
 	const Files = await loadFiles("commands")
 
 	Files.forEach((file) => {
-		//testOnly need implementation yet
-		//developer commands should only be visible in the servers
 		const command = require(file)
 		client.commands.set(command.data.name, command)
 
-		commandsArray.push(command.data.toJSON())
+		if (instance.config.testOnly || command.developer) {
+			commandsGuild.push(command.data.toJSON())
+		} else {
+			commandsArray.push(command.data.toJSON())
+		}
 
 		console.log(`Command: ${command.data.name} ✅`)
 	})
 
 	client.application.commands.set(commandsArray)
+	client.application.commands.set(commandsGuild, instance.config.testGuild)
 
-	fetch(
-		`https://discordbotlist.com/api/v1/bots/${instance.config.botId}/commands`,
-		{
-			method: "POST",
-			headers: {
-				Authorization: process.env.Authorization2,
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(commandsArray),
-		}
-	)
-		.then((response) => response.json())
-		.then((response) => console.log(JSON.stringify(response)))
+	if (commandsArray.length > 0) {
+		fetch(
+			`https://discordbotlist.com/api/v1/bots/${instance.config.botId}/commands`,
+			{
+				method: "POST",
+				headers: {
+					Authorization: process.env.Authorization2,
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(commandsArray),
+			}
+		)
+			.then((response) => response.json())
+			.then((response) => console.log(JSON.stringify(response)))
+	}
 }
 
 module.exports = { loadCommands }
