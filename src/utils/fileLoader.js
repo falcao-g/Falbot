3d6a4616a53b406454ba6db7d8c4a4ae110e2400
Falcão <gabriel.p.falcao50@gmail.com>
2023-02-11 02:45:31 -0400

    optimized a bit

--- src/utils/fileLoader.js
+++ src/utils/fileLoader.js
@@ -7,20 +7,17 @@ async function loadFiles(dirName) {
 	const basePath = `${process.cwd().replace(/\\/g, "/")}/src/${dirName}`
 
 	let files = []
-	const readDirectory = async (dir) => {
-		const items = await readdir(dir)
-		for (const item of items) {
-			const itemPath = `${dir}/${item}`
-			const itemStat = await stat(itemPath)
-			if (itemStat.isDirectory()) {
-				await readDirectory(itemPath)
-			} else if (itemPath.endsWith(".js")) {
-				files.push(itemPath)
-				delete require.cache[require.resolve(itemPath)]
-			}
+	const items = await readdir(basePath)
+	for (const item of items) {
+		const itemPath = `${basePath}/${item}`
+		const itemStat = await stat(itemPath)
+		if (itemStat.isDirectory()) {
+			await readDirectory(itemPath)
+		} else if (itemPath.endsWith(".js")) {
+			files.push(itemPath)
+			delete require.cache[require.resolve(itemPath)]
 		}
 	}
-	await readDirectory(basePath)
 
 	return files
 }
