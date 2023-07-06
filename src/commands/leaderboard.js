f16788e8e4c174d796cfd582579f7ad483d2bbe8
Falcão <gabriel.p.falcao50@gmail.com>
2023-07-05 23:26:45 -0400

    refatoração

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -84,138 +84,108 @@ module.exports = {
 				)
 		),
 	execute: async ({ client, user, guild, interaction, instance }) => {
-		await interaction.deferReply()
-		var rank = []
-		const scope = interaction.options.getString("type")
-		const type = interaction.options.getSubcommand()
+		try {
+			await interaction.deferReply()
+			var rank = []
+			const scope = interaction.options.getString("type")
 
-		var embed1 = new EmbedBuilder().setColor("#206694").setFooter({ text: "by Falcão ❤️" })
-		var embed2 = new EmbedBuilder().setColor("#206694").setFooter({ text: "by Falcão ❤️" })
-		var embed3 = new EmbedBuilder().setColor("#206694").setFooter({ text: "by Falcão ❤️" })
+			var embed1 = new EmbedBuilder().setColor("#206694").setFooter({ text: "by Falcão ❤️" })
+			var embed2 = new EmbedBuilder().setColor("#206694").setFooter({ text: "by Falcão ❤️" })
+			var embed3 = new EmbedBuilder().setColor("#206694").setFooter({ text: "by Falcão ❤️" })
 
-		embeds = [embed1, embed2, embed3]
-
-		if (type == "falcoins") {
-			if (scope === "server") {
-				users = await instance.userSchema.find({}).sort({ falcoins: -1 })
-
-				for (useri of users) {
-					if (await getMember(guild, useri["_id"])) {
-						rank.push(useri)
-					}
-				}
-			} else {
-				rank = await instance.userSchema.find({}).sort({ falcoins: -1 }).limit(30)
-			}
-		} else if (type == "rank") {
-			if (scope === "server") {
-				users = await instance.userSchema.find({}).sort({ rank: -1 })
-
-				for (useri of users) {
-					if (await getMember(guild, useri["_id"])) {
-						rank.push(useri)
-					}
-				}
-			} else {
-				rank = await instance.userSchema.find({}).sort({ rank: -1 }).limit(30)
-			}
-		} else if (type == "wins") {
-			if (scope === "server") {
-				users = await instance.userSchema.find({}).sort({ vitorias: -1 })
-
-				for (useri of users) {
-					if (await getMember(guild, useri["_id"])) {
-						rank.push(useri)
-					}
-				}
-			} else {
-				rank = await instance.userSchema.find({}).sort({ vitorias: -1 }).limit(30)
-			}
-		}
-
-		for (let i = 0; i < rank.length; i++) {
-			if (i <= 9) {
-				a = 0
-			} else if (i <= 19) {
-				a = 1
-			} else {
-				a = 2
-			}
-			try {
-				if (scope === "server") {
-					member = await getMember(guild, rank[i]["_id"])
-					username = member.displayName
-				} else {
-					user = await client.users.fetch(rank[i]["_id"])
-					username = user.username
-				}
-
-				embeds[a].addFields({
-					name:
-						type == "wins"
-							? `${i + 1}º - ${username} ` + instance.getMessage(interaction, "VITORIAS").toLowerCase() + ":"
-							: `${i + 1}º - ${username} ${type}:`,
-					value:
-						type == "rank"
-							? `${instance.getMessage(interaction, rank[i][type])}`
-							: `${type == "wins" ? rank[i]["vitorias"] : format(rank[i][type])}`,
-				})
-			} catch {
-				embeds[a].addFields({
-					name:
-						type === "wins"
-							? `${i + 1}º - Unknown user ` + instance.getMessage(interaction, "VITORIAS").toLowerCase() + ":"
-							: `${i + 1}º - Unknown user ${type}:`,
-					value:
-						type === "rank"
-							? `${instance.getMessage(interaction, rank[i][type])}`
-							: `${type == "wins" ? rank[i]["vitorias"] : format(rank[i][type])}`,
-				})
-			}
-		}
-
-		if (embed3.data.fields) {
 			embeds = [embed1, embed2, embed3]
-		} else if (embed2.data.fields) {
-			embeds = [embed1, embed2]
-		} else {
-			embeds = [embed1]
-		}
 
-		if (embeds.length > 1) {
-			for (let i = 0; i < embeds.length; i++) {
-				if (scope == "server") {
-					embeds[i].setTitle(`${instance.getMessage(interaction, "LEADERBOARD_SERVER_TITLE")} - ${i + 1}/3`)
-				} else {
-					embeds[i].setTitle(`${instance.getMessage(interaction, "LEADERBOARD_GLOBAL_TITLE")} - ${i + 1}/3`)
-				}
+			enums = {
+				falcoins: "falcoins",
+				rank: "rank",
+				wins: "vitorias",
 			}
 
-			const paginator = paginate()
-			paginator.add(...embeds)
-			const ids = [`${Date.now()}__left`, `${Date.now()}__right`]
-			paginator.setTraverser([
-				new ButtonBuilder().setEmoji("⬅️").setCustomId(ids[0]).setStyle("Secondary"),
-				new ButtonBuilder().setEmoji("➡️").setCustomId(ids[1]).setStyle("Secondary"),
-			])
-			const message = await interaction.editReply(paginator.components())
-			message.channel.createMessageComponentCollector().on("collect", async (i) => {
-				if (i.customId === ids[0]) {
-					await paginator.back()
-					await i.update(paginator.components())
-				} else if (i.customId === ids[1]) {
-					await paginator.next()
-					await i.update(paginator.components())
+			const type = enums[interaction.options.getSubcommand()]
+
+			if (scope === "server") {
+				users = await instance.userSchema.find({}).sort({ [type]: -1 })
+
+				for (useri of users) {
+					if (await getMember(guild, useri["_id"])) {
+						rank.push(useri)
+					}
 				}
-			})
-		} else {
-			if (scope == "server") {
-				embed1.setTitle(instance.getMessage(interaction, "LEADERBOARD_SERVER_TITLE"))
 			} else {
-				embed1.setTitle(instance.getMessage(interaction, "LEADERBOARD_GLOBAL_TITLE"))
+				rank = await instance.userSchema
+					.find({})
+					.sort({ [type]: -1 })
+					.limit(30)
 			}
 
-			await interaction.editReply({ embeds: [embeds[0]] })
+			for (let i = 0; i < rank.length; i++) {
+				a = Math.min(Math.floor(i / 10), 2)
+
+				if (scope === "server") {
+					try {
+						member = await getMember(guild, rank[i]["_id"])
+						username = member.displayName
+					} catch {
+						username = "Unknown user"
+					}
+				} else {
+					try {
+						user = await client.users.fetch(rank[i]["_id"])
+						username = user.username
+					} catch {
+						username = "Unknown user"
+					}
+				}
+
+				embeds[a].addFields({
+					name: `${i + 1}º - ${username} ${type}:`,
+					value: type == "rank" ? `${instance.getMessage(interaction, rank[i][type])}` : `${format(rank[i][type])}`,
+				})
+			}
+
+			if (embed3.data.fields) {
+				embeds = [embed1, embed2, embed3]
+			} else if (embed2.data.fields) {
+				embeds = [embed1, embed2]
+			} else {
+				embeds = [embed1]
+			}
+
+			if (embeds.length > 1) {
+				for (let i = 0; i < embeds.length; i++) {
+					embeds[i].setTitle(
+						`${instance.getMessage(interaction, `LEADERBOARD_${scope.toUpperCase()}_TITLE`)} - ${i + 1}/3`
+					)
+				}
+
+				const paginator = paginate()
+				paginator.add(...embeds)
+				const ids = [`${Date.now()}__left`, `${Date.now()}__right`]
+				paginator.setTraverser([
+					new ButtonBuilder().setEmoji("⬅️").setCustomId(ids[0]).setStyle("Secondary"),
+					new ButtonBuilder().setEmoji("➡️").setCustomId(ids[1]).setStyle("Secondary"),
+				])
+				const message = await interaction.editReply(paginator.components())
+				message.channel.createMessageComponentCollector().on("collect", async (i) => {
+					if (i.customId === ids[0]) {
+						await paginator.back()
+						await i.update(paginator.components())
+					} else if (i.customId === ids[1]) {
+						await paginator.next()
+						await i.update(paginator.components())
+					}
+				})
+			} else {
+				embed1.setTitle(instance.getMessage(interaction, `LEADERBOARD_${scope.toUpperCase()}_TITLE`))
+				await interaction.editReply({ embeds: [embeds[0]] })
+			}
+		} catch (error) {
+			console.error(`leaderboard: ${error}`)
+			interaction.editReply({
+				content: instance.getMessage(interaction, "EXCEPTION"),
+				embeds: [],
+				components: [],
+			})
 		}
 	},
 }
