087ef280eaa2ca69ca43e52fc6b4bc8d1ce5e281
Falcão <gabriel.p.falcao50@gmail.com>
2023-09-29 16:00:51 -0400

    fix: readDirectory() isnt a function

--- src/utils/fileLoader.js
+++ src/utils/fileLoader.js
@@ -12,7 +12,8 @@ async function loadFiles(dirName) {
 		const itemPath = `${basePath}/${item}`;
 		const itemStat = await stat(itemPath);
 		if (itemStat.isDirectory()) {
-			await readDirectory(itemPath);
+			const subFiles = await loadFiles(`${dirName}/${item}`);
+			files.push(...subFiles);
 		} else if (itemPath.endsWith('.js')) {
 			files.push(itemPath);
 			delete require.cache[require.resolve(itemPath)];
